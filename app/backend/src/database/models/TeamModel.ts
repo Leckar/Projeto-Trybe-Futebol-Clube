4b00a4bc01764703399d855966b32d13cb6f4025
Leckar <gusleckar@gmail.com>
2023-03-28 15:15:47 -0300

    Update: Created a Model for Teams

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,30 @@
+import {
+  Model,
+  INTEGER,
+  STRING,
+} from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    allowNull: false,
+    field: 'team_name',
+    type: STRING(100),
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
