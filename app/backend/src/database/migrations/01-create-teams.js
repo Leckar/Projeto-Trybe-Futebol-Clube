eaaf01e29fbb3548f5dba05cbf20de7b3443ae27
Leckar <gusleckar@gmail.com>
2023-03-28 15:01:45 -0300

    Update: Created Teams migration and Utils folder

---
+++ app/backend/src/database/migrations/01-create-teams.js
@@ -0,0 +1,19 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('teams');
+  },
+};
---
+++ app/backend/src/utils/HttpStatuses.ts
@@ -0,0 +1,12 @@
+const statusPack = {
+  ok: 200,
+  created: 201,
+  noContent: 204,
+  badRequest: 400,
+  unauthorized: 401,
+  notFound: 404,
+  conflict: 409,
+  unprocessableEntity: 422,
+};
+
+export default statusPack;
